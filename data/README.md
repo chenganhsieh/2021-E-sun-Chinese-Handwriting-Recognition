698b4c80acc7042d44305bb4933e9b7f9f5b1cda
Jackson <pigoo833@gmail.com>
2021-05-14 03:30:54 +0800

    upgrade download url

--- data/README.md
+++ data/README.md
@@ -1,7 +1,7 @@
 # Data Augmentation
 分成兩種data: clean 以及 dirty
-* clean: 官網的800字轉成黑白
-* dirty: AIteam的4000多字轉成官網的髒背景
+* clean: 官網的800字轉成黑白 444.4MB
+* dirty: AIteam的4000多字轉成官網的髒背景 2.96GB
 * 資料夾格式：
     * handwritten_data
         * clean
@@ -28,23 +28,23 @@
 
 ## 下載資料
 如果加上clean參數會連同clean一起下載
-'''shell
+```shell
 bash download.sh clean(optonal)
-'''
+```
 
 
 ## decompress.py
 --data_dir ./Traditional-Chinese-Handwriting-Dataset/data
 --output_dir ./handwritten_data
 --source 指定aiteam資料或是官網資料
-'''shell
+```shell
 python3 decompress.py --source AITEAMorDefault
-'''
+```
 ## augmentation.py
 --type ['donoise', 'doclean']
-'''shell
+```shell
 python3 augmentation.py
-'''
+```
 
 ## 待處理
-* 官網每個data dim目前都不相同
+* 官網每個data dim目前都不固定
--- data/download.sh
+++ data/download.sh
@@ -1,8 +1,8 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O dirty.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1GlFbs305hnh1p9vmROVWjAQKs3Zm5XZE' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1GlFbs305hnh1p9vmROVWjAQKs3Zm5XZE" -O dirty.zip && rm -rf /tmp/cookies.txt
 unzip dirty.zip -d ./handwritten_data
 
-if [ $1 == "clean"]
+if [ "$1" == "clean" ]
 then
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O dirty.zip && rm -rf /tmp/cookies.txt
-unzip dirty.zip -d ./handwritten_data
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1BMPapHnQ4LnvD_1qwEYDdFsMXHaH0_ed' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1BMPapHnQ4LnvD_1qwEYDdFsMXHaH0_ed" -O clean.zip && rm -rf /tmp/cookies.txt
+unzip clean.zip -d ./handwritten_data
 fi
